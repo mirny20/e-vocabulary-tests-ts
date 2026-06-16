65646c7123be0cb2a61200250586dabdfaad3264
mirny20 <urbanvo@proton.me>
2026-06-16 18:05:43 +0300

    reworked authentication process from UI to API

--- tests/setupStorageState/mainUserAuth.setup.ts
+++ tests/setupStorageState/mainUserAuth.setup.ts
@@ -1,21 +1,50 @@
 import { test as setup } from '@playwright/test';
 import { env } from '../../src/common/config/env';
-import { AuthPage } from '@pages/AuthPage'
-import { HomePage } from '@pages/HomePage';
 import path from 'path';
+import * as fs from 'fs';
 
 export const authFile = path.join(__dirname, '..', '..', 'playwright', '.auth', 'mainUser.json');
 
-setup('Authenticate main user', async ({ page }) => {
-  const authPage = new AuthPage(page);
-  const homePage = new HomePage(page);
+setup('Authenticate main user', async ({ request }) => {
+  const requestBodyData = {
+    "email": "",
+    "username": env.users.mainUser.username,
+    "password": env.users.mainUser.password
+  }
 
-  await authPage.open();
-  await authPage.performLogin(
-    env.users.mainUser.username,
-    env.users.mainUser.password
-  );
-  await homePage.waitForHomePageToBeLoaded();
+  const response = await request.post('https://build-wheat-rho.vercel.app/api/auth/login', {
+    data: requestBodyData,
+  });
 
-  await page.context().storageState({path: authFile});
+  if (!response.ok()) {
+    throw new Error(`Auth failed: ${response.status()} ${response.statusText()}`);
+  }
+
+  const responseBody = await response.json();
+
+  const storage_state = {
+    "cookies": [],
+    "origins": [
+      {
+        "origin": "https://e-vocabulary.vercel.app",
+        "localStorage": [
+          {
+            "name": "token",
+            "value": responseBody.token,
+          },
+          {
+            "name": "refreshToken",
+            "value": responseBody.refreshToken,
+          },
+          {
+            "name": "maxLengthQuote",
+            "value": "91"
+          }
+        ]
+      }
+    ]
+  }
+
+  fs.mkdirSync(path.dirname(authFile), {recursive: true});
+  fs.writeFileSync(authFile, JSON.stringify(storage_state, null, 2));
 });
